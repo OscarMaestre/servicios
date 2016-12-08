ae095aaa0a1545071499e6ea3d00ff79252374f4
Oscar G.G <profesor.oscar.gomez@gmail.com>
2016-12-08 16:11:18 +0100

    Incluido workspace

---
+++ workspace/PlantillaAplicacionMultihilo/src/com/ies/plantilla/Lanzador.java
@@ -0,0 +1,39 @@
+package com.ies.plantilla;
+
+public class Lanzador {
+
+	public Lanzador() {
+		/* Poner aquí todo el código que
+		 * se necesite para inicializar los atributos
+		 */
+	}
+	
+	/* Nadie de fuera puede llamar a este método. Ni el main*/
+	protected void crearRecursosCompartidos(){
+		/* Crear aquí los recursos compartidos. Esto se
+		 * hará ANTES DE LANZAR LOS HILOS */
+	}
+	/* Nadie de fuera puede llamar a este método. Ni el main*/
+	protected void crearHilos(){
+		/* Crear aquí los hilos. Esto se ejecutará
+		 * DESPUES DE CREAR LOS RECURSOS COMPARTIDOS */
+		
+	}
+	
+	/* Este método pone en marcha todo el proceso...*/
+	public void lanzar(){
+		/* Para ello PRIMERO CREA LOS RECURSOS COMPARTIDOS*/
+		crearRecursosCompartidos();
+		
+		/* Y despues crea los hilos, pasándoles, si es necesario
+		 * el o los objetos compartidos */
+		crearHilos();
+	}
+
+	public static void main(String[] args) {
+		Lanzador lanzador=new Lanzador();
+		lanzador.lanzar();
+
+	}
+
+}
---
+++ workspace/PlantillaAplicacionMultihilo/src/com/ies/plantilla/RecursoCompartido.java
@@ -0,0 +1,27 @@
+package com.ies.plantilla;
+
+
+/* Ejemplo de recurso compartido y de su implementación*/
+
+public class RecursoCompartido {
+	/* Todo objeto compartido tendrá uno o varios valores
+	 * de importancia. Deberían declarse aquí */
+	int valorImportante;
+	public RecursoCompartido() {
+		/* El constructor debería inicializar todos los valores
+		 * de importancia y todos los que necesite para
+		 * funcionar
+		 */
+	}
+	/* Cualquier modificación o consulta
+	 * de un valor compartido por varios hilos
+	 * debería usar "synchronized" */
+	public synchronized void setValor(int nuevoValor){
+		valorImportante=nuevoValor;
+	}
+	public synchronized int getValor(){
+		return valorImportante;
+	}
+	
+
+}
